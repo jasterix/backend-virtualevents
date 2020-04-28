dd75759e48f8bbefe00d7b90c917792074e2327e
Jasterix <ljcalixte@gmail.com>
2020-04-28 01:12:52 -0400

    seeded db with first 21 events

--- api/models/event.js
+++ api/models/event.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 
 const eventSchema = mongoose.Schema(
   {
-    _id: mongoose.Schema.Types.ObjectId,
     title: String,
     date: String,
     startDate: Date,
